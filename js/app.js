20ac60b8b81c59314035ecfdf6355663be22f38b
TimM32 <timmaupin@live.com>
2023-03-13 13:05:05 -0500

    add js code to app.js file

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let userName = prompt("What is your name?");
+console.log("user name: ", userName);
+let confirmMessage = confirm("Do you want to continue?");
+console.log("confirm message: ", confirmMessage);
+
+let response = getFavoriteSport();
+
+function getFavoriteSport() {
+    let userSport = prompt("what's your favorite movie?");
+    userSport = userSport.toLowerCase();
+    let response;
+
+    if (userSport === "baseball") {
+        response = "Awesome!";
+    } else if (userSport === "football") {
+        response = "Best Sport All-time!!";
+    } else if (userSport === "basketball") {
+        response = "Ah a classic!";
+    } else {
+        response = "Great Choice!";
+    }
+    console.log("response", response);
+    return response;
+}
+document.write(response);
